da0fd1249b10d0c5fc75821ea2d30583d1cedb1a
Francisco Ricardo <78240032+franciscoRic@users.noreply.github.com>
2021-03-08 10:00:14 -0400

    Crud Funcionarios

--- apps/empresa/models.py
+++ apps/empresa/models.py
@@ -11,4 +11,3 @@ class Empresa(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
- 
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
 
-def home(request):
-    return HttpResponse('Ola')
 
+class FuncionariosList(ListView):
+    model = Funcionario
