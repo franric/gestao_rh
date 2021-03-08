d41732af1801d72bbb2fa17457cad56f539e5a82
Francisco Ricardo <78240032+franciscoRic@users.noreply.github.com>
2021-03-08 16:36:36 -0400

    Crud Funcionario filter empresa

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -6,3 +6,8 @@ from .models import Funcionario
 
 class FuncionariosList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
