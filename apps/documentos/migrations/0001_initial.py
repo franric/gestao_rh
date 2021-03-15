ee311279fffe0fa557757272144714534adead4b
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-03-14 21:30:00 -0400

    list horas extras

--- apps/documentos/migrations/0001_initial.py
+++ apps/documentos/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Documento',
+            name='Documentos',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('descricao', models.CharField(max_length=100)),
