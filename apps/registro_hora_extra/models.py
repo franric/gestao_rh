0ae2c7c6b87a42a1a58c4743811ee5d4104cacd1
Francisco Ricardo <ricardo.souza1910@gmail.com>
2021-03-22 10:19:36 -0400

    Update de Hora extra

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from apps.funcionarios.models import Funcionario
 
 
@@ -7,5 +9,9 @@ class RegistroHoraExtra(models.Model):
     funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     horas = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def get_absolute_url(self):
+        return reverse('list_hora_extra')
+
+    
     def __str__(self):
         return self.motivo
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import (
-    ListView
+    ListView, UpdateView
 )
 
 from .models import RegistroHoraExtra
@@ -12,3 +12,8 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
